abd34ecfef59afc34267322392c893f99107f213
AndileSithole <149889136+AndileSithole08@users.noreply.github.com>
2024-09-09 16:26:21 +0200

    Delete utility-project directory

--- utility-project/check_availability.py
+++
@@ -1,9 +0,0 @@
-# check_availability.py
-import json
-
-# Dummy data representing available zones
-data = {
-    "available_zones": ["us-east-1a", "us-east-1b", "us-east-1c"]
-}
-
-print(json.dumps(data))
